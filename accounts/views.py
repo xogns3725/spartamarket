d6124372076199522ac4e02185441d91dccd2726
xogns3725 <xogns3725@naver.com>
2024-04-15 10:45:21 +0900

    login func

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
 from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 def signup(request):
     if request.method == "POST":
@@ -13,3 +14,14 @@ def signup(request):
         form = CustomUserCreationForm()
     context = {"form": form}
     return render(request, "accounts/signup.html", context)
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect("products:products")
+    else:
+        form = AuthenticationForm()
+    context = {"form": form}
+    return render(request, "accounts/login.html", context)
