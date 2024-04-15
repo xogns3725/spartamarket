a804ee8836fe0c1e1e230a8469159068f96cf17e
xogns3725 <xogns3725@naver.com>
2024-04-15 13:29:11 +0900

    follow func

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import get_user_model
+from django.views.decorators.http import require_POST
 
 
 def users(request):
@@ -14,3 +15,14 @@ def profile(request, username):
     return render(request, "users/profile.html", context)
 
 
+@require_POST
+def follow(request, user_id):
+    if request.user.is_authenticated:
+        member = get_object_or_404(get_user_model(), pk=user_id)
+        if request.user != member:
+            if request.user in member.followers.all():
+                member.followers.remove(request.user)
+            else:
+                member.followers.add(request.user)
+        return redirect("users:profile", member.username)
+    return redirect("accounts:login")
