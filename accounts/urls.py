9bd23804b9ac4c3901d9fd856c8962791da0c358
xogns3725 <xogns3725@naver.com>
2024-04-15 10:48:46 +0900

    logout func

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "accounts"
 urlpatterns = [
     path("login/", views.login, name="login"),
-    # path("logout/", views.logout, name="logout"),
+    path("logout/", views.logout, name="logout"),
     path("signup/", views.signup, name="signup"),
     # path("delete/", views.delete, name="delete"),
     # path("update/", views.update, name="update"),
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 from .forms import CustomUserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
+from django.views.decorators.http import require_POST
 
 def signup(request):
     if request.method == "POST":
@@ -15,6 +17,7 @@ def signup(request):
     context = {"form": form}
     return render(request, "accounts/signup.html", context)
 
+
 def login(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
@@ -25,3 +28,10 @@ def login(request):
         form = AuthenticationForm()
     context = {"form": form}
     return render(request, "accounts/login.html", context)
+
+
+@require_POST
+def logout(request):
+    if request.method == "POST":
+        auth_logout(request)
+    return redirect("products:products")
