07179d01a48a8aa226625a108fe8ba44ed3536ee
xogns3725 <xogns3725@naver.com>
2024-04-18 09:42:07 +0900

    liked_list, unlike func

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.users, name="users"),
     path("profile/<str:username>/", views.profile, name="profile"),
     path("<int:user_id>/follow/", views.follow, name="follow"),
+    path("unlike/<int:pk>", views.unlike, name='unlike'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import get_user_model
 from django.views.decorators.http import require_POST
+from products.models import Product
 
 
 def users(request):
@@ -9,8 +10,12 @@ def users(request):
 
 def profile(request, username):
     member = get_object_or_404(get_user_model(), username=username)
+    user = get_user_model().objects.get(username=username)
+    liked_products = Product.objects.filter(like_users=user)
     context = {
         "member": member,
+        'user': user,
+        'liked_products': liked_products
     }
     return render(request, "users/profile.html", context)
 
@@ -26,3 +31,13 @@ def follow(request, user_id):
                 member.followers.add(request.user)
         return redirect("users:profile", member.username)
     return redirect("accounts:login")
+
+
+@require_POST
+def unlike(request, pk):
+    if request.user.is_authenticated:
+        product = get_object_or_404(Product, pk=pk)
+        product.like_users.remove(request.user)
+        return redirect('users:profile', username=request.user.username)
+    else:
+        return redirect("accounts:login")
